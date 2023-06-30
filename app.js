880f624d00d008ca4cfb4085429b6a17df3528d2
Reese <reeseknott@gmail.com>
2023-06-29 20:11:01 -0700

    update

---
+++ app.js
@@ -0,0 +1,68 @@
+let items = document.querySelectorAll('#recipeCarousel .carousel-item')
+
+  items.forEach((el) => {
+      const minPerSlide = 4
+      let next = el.nextElementSibling
+      for (var i=0; i<minPerSlide; i++) {
+          if (!next) {
+            interval: 0,
+      
+              next = items[0]
+            }
+          let cloneChild = next.cloneNode(true)
+          el.appendChild(cloneChild.children[0])
+          next = next.nextElementSibling
+      }
+  })
+  
+
+
+
+  $(function () {
+    var bindDatePicker = function() {
+         $(".date").datetimepicker({
+         format:'YYYY-MM-DD',
+             icons: {
+                 time: "fa fa-clock-o",
+                 date: "fa fa-calendar",
+                 up: "fa fa-arrow-up",
+                 down: "fa fa-arrow-down"
+             }
+         }).find('input:first').on("blur",function () {
+             // check if the date is correct. We can accept dd-mm-yyyy and yyyy-mm-dd.
+             // update the format if it's yyyy-mm-dd
+             var date = parseDate($(this).val());
+ 
+             if (! isValidDate(date)) {
+                 //create date based on momentjs (we have that)
+                 date = moment().format('YYYY-MM-DD');
+             }
+ 
+             $(this).val(date);
+         });
+     }
+    
+    var isValidDate = function(value, format) {
+         format = format || false;
+         // lets parse the date to the best of our knowledge
+         if (format) {
+             value = parseDate(value);
+         }
+ 
+         var timestamp = Date.parse(value);
+ 
+         return isNaN(timestamp) == false;
+    }
+    
+    var parseDate = function(value) {
+         var m = value.match(/^(\d{1,2})(\/|-)?(\d{1,2})(\/|-)?(\d{4})$/);
+         if (m)
+             value = m[5] + '-' + ("00" + m[3]).slice(-2) + '-' + ("00" + m[1]).slice(-2);
+ 
+         return value;
+    }
+    
+    bindDatePicker();
+  });
+
+
